02a5ac42adb1dc3aa224bccf38d46ff14611a6f4
anima <ccd230003@utdallas.edu>
2026-05-10 15:28:22 -0500

    implemented the screen set up

---
+++ src/project.py
@@ -0,0 +1,11 @@
+import pygame
+import sys
+
+pygame.init()
+
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("I Want to Be Friends!")
+clock = pygame.time.Clock()
+
