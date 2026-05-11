c2ea7589716cb2668c28fb805dd422748935720b
anima <ccd230003@utdallas.edu>
2026-05-10 19:23:12 -0500

    added section for color variables

--- src/project.py
+++ src/project.py
@@ -2,10 +2,15 @@ import pygame
 import sys
 
 pygame.init()
-
+#screen setup
 SCREEN_WIDTH = 1920
 SCREEN_HEIGHT = 1080
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("I Want to Be Friends!")
 clock = pygame.time.Clock()
+#colors
+WHITE = (255, 255, 255)
+BLK = (0, 0, 0)
+DARK_GRAY = (50, 50, 50)
+LIGHT_GRAY = (200, 200, 200)
 
