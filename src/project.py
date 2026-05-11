c1ecc4b756da4ad71c3d7d64d6bfedf63b3d1f2d
anima <ccd230003@utdallas.edu>
2026-05-10 23:16:28 -0500

    made a method for script which is started but not done, one for resetting, one for the game to begin typing its current line, one for constant updates of the text, and one for clickable buttons.

--- src/project.py
+++ src/project.py
@@ -23,7 +23,65 @@ font_text = pygame.font.Font(None, 28)
 dialogue_box_rect = pygame.Rect(50, SCREEN_HEIGHT - 150, SCREEN_WIDTH - 100, 120)
 dialogue_padding = 10
 
+class VisualNovel():
 
+    def __init__(self):
+        self.script = [{"char": "Finn", "text": "He's so cool..."},
+                       {"char": "Finn", "text": "I should talk to him. Like, now. Like, right now."},
+                       {"char": "Tito", "text": "Can I help you?"},
+                       {"char": "Finn", "text": "Huh?", "choices": [("Huh?", 5), ("Me?", 6)]},
+                       
+                       ]
+        self.current_line = 0
+        self.display_text = ""
+        self.text_idx = 0
+        self.text_speed = 30
+        self.last_update = pygame.time.get_ticks()
+        self.done_typing = False
+        self.showing_choices = False
+        self.choices_rects = []
+        self.ending = False
+
+    def reset(self):
+        self.current_line = 0
+        self.display_text = ""
+        self.text_idx = 0
+        self.done_typing = False
+        self.showing_choices = False
+        self.ending = False
+        self.start_typing()
+
+    def start_typing(self):
+        line = self.script[self.current_line]
+        self.display_text = ""
+        self.text_idx = 0
+        self.done_typing = False
+        self.last_update = pygame.time.get_ticks()
+        self.showing_choices = False
+    
+    def update_text(self):
+        if not self.done_typing and not self.showing_choices:
+            current_time = pygame.time.get_ticks
+            if current_time - self.last_update >= self.text_speed:
+                line = self.script[self.current_line]
+                if self.text_idx < len(line["text"]):
+                    self.display_text += line["text"][self.text_idx]
+                    self.text_idx += 1
+                    self.last_update = current_time
+                else:
+                    self.done_typing = True
+                    if "choices" in line and not self.ending:
+                        self.showing_choices = True
+                        self.create_choice_buttons()
+
+    def create_choice_buttons(self):
+        self.choices_rects = []
+        line = self.script[self.current_line]
+        start_y = SCREEN_HEIGHT - 130
+
+        for i, (choice_txt, _) in enumerate(line["choices"]):
+            btn_rect = pygame.Rect(70, start_y + i * 40, SCREEN_WIDTH - 140, 35)
+        self.choices_rects.append((btn_rect, choice_txt, i))
 
 def main():
     vn = VisualNovel()
