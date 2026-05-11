6b3435b6886d4570f0606d8e5130a961a5fa1ab3
anima <ccd230003@utdallas.edu>
2026-05-10 19:39:10 -0500

    implemented game loop, added if name = main

--- src/project.py
+++ src/project.py
@@ -14,3 +14,35 @@ BLK = (0, 0, 0)
 DARK_GRAY = (50, 50, 50)
 LIGHT_GRAY = (200, 200, 200)
 
+def main():
+    vn = VisualNovel()
+    vn.start_typing()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                if vn.ending:
+                    vn = VisualNovel()
+                    vn.start_typing()
+                elif vn.showing_choices:
+                    vn.handle_choice_click(event.pos)
+                else:
+                    vn.advance()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE or event.key == pygame.K_RETURN:
+                    if vn.ending:
+                        vn = VisualNovel()
+                        vn.start_typing()
+                    elif not vn.showing_choices:
+                        vn.advance()
+        vn.update_text()
+        vn.draw(screen)
+        pygame.display.flip()
+        clock.tick(60)
+    pygame.quit()
+    sys.exit()
+if __name__ == "__main__":
+    main()
